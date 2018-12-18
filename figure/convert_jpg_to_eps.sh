033e844586f01dbc52ade17a5621b120061cd7f3
calm0815 <calm0815@example.com>
2018-12-19 07:51:27 +0900

    edit something

--- figure/convert_jpg_to_eps.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-mkdir -p eps
-for f in *.jpg; do
-convert $f eps2:eps/${f%.jpg}.eps;
-done
--- figure/convert_png_to_eps.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-mkdir -p eps
-for f in *.png; do
-convert $f eps2:eps/${f%.png}.eps;
-done
